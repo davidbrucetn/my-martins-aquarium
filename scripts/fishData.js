1957bdf3963f79cf8564657f54f480018da10cc6
davidbrucetn <severian37@gmail.com>
2020-06-02 09:31:19 -0500

    Added fishData.js

---
+++ scripts/fishData.js
@@ -0,0 +1,56 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "algae",
+        species: "Guppy",
+        length: "3 cm",
+        location: "Caribbean"
+
+    },
+    {
+        name: "Allison",
+        food: "Brine Shrimp",
+        species: "Angelfish",
+        length: "6 inches",
+        location: "South America"
+
+    },
+    {
+        name: "Nancy",
+        food: "Bloodworms",
+        species: "Neon Tetra",
+        length: "3 cm",
+        location: "Amazon Basin"
+
+    },
+    {
+        name: "Maggy",
+        food: "Brine Shrimp",
+        species: "Mollie",
+        length: "3 inches",
+        location: "Tropical Estuaries"
+
+    },
+    {
+        name: "Plato",
+        food: "Fruit Flies",
+        species: "Platy",
+        length: "2.2 inches",
+        location: "South America"
+
+    },
+    {
+        name: "Delia",
+        food: "Bloodworms",
+        species: "Zebra Danios",
+        length: "1.75 inches",
+        location: "Myanmar"
+
+    }
+]
