fa1406f564a5ddf669ab93808ba4cbf0bbe530bd
davidbrucetn <severian37@gmail.com>
2020-06-04 09:27:16 -0500

    Backup before API

--- scripts/fish/fishList.js
+++ scripts/fish/fishList.js
@@ -1,62 +1,29 @@
-/*
-    Generating a list of fish in HMTL reprensentations and putting it in the browser
-*/
-// Function to show holy fish in the browser
-const showHolyFish = () => {
-    const fishObjectsArray = mostHolyFish()
-    const rank = "holy"
-    for (const currentFishObject of fishObjectsArray) {
-        const fishHTML = fishConverter(currentFishObject,rank);
-        const fishArticleElement = document.querySelector(".fishList");
-        fishArticleElement.innerHTML += fishHTML
-    }
-}
-
-// Function to show soldier fish in the browser
-const showSoldierFish = () => {
-    const fishObjectsArray = soldierFish()
-    const rank = "soldier"
-    for (const currentFishObject of fishObjectsArray) {
-        const fishHTML = fishConverter(currentFishObject,rank);
-        const fishArticleElement = document.querySelector(".fishList");
-        fishArticleElement.innerHTML += fishHTML
-    }
-
-}
-
-// Function to show common fish in the browser
-const showCommonFish = () => {
-    const fishObjectsArray = commonFish()
-    const rank = "common"
-    for (const currentFishObject of fishObjectsArray) {
-        const fishHTML = fishConverter(currentFishObject,rank);
-        const fishArticleElement = document.querySelector(".fishList");
-        fishArticleElement.innerHTML += fishHTML
-    }
-
-}
-
-
 const fishList = () => {
-    showHolyFish();
-    showSoldierFish();
-    showCommonFish();
-
-
+    
+    const holy = geFilterFish("holy")
+    displayFish(holy,"holy");
+    
+    const soldier = geFilterFish("soldier")
+    displayFish(soldier,"soldier");
+    
+    const common = geFilterFish("common");
+    displayFish(common,"common");
 }
 
-/*
-const fishList = () => {
-        // Iterate the collection of fish objects 
-    for (const currentFishObject of fishCollection) {
 
-        // convert the current fish to HTML
-        const currentFishObject = fishConverter(currentFishObject);
-        
-        // find the <article> element in index.html for the fish list
-        const fishArticleElement = document.querySelector(".fishList");
-        // build concatenated HTML list of fish from fishCollection
-        fishArticleElement.innerHTML += fishHTML
+
+const displayFish = (fishObjectsArray,rank) => {
+
+    for (const currentFishObject of fishObjectsArray) {
+        //const fishHTML = fishConverter(currentFishObject,rank);
+        //const fishArticleElement = document.querySelector(".fishList");
+        //fishArticleElement.innerHTML += fishHTML
+
+        // document element has html inserted from fishconverter w/o const variable
+        document.querySelector(".fishList").innerHTML += fishConverter(currentFishObject,rank);
     }
 }
-*/
+
+//   function -------> (params)   -> array.forEach(callback --> HTML ELEMENT <--return from (ObjData-to-HTML Converter)
+//const displayFish = (fish,rank) => fish.forEach(f => document.querySelector(".fishList").innerHTML += fishConverter(f,rank)
+//)
